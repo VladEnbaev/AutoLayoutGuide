be418e63398964d435eedb392d9290e1fa82f4aa
VladGradskiy <enbaev.vlad4@gmail.com>
2023-03-11 18:19:07 +0300

    last changes

--- AutoLayoutGuide/ParentViewController.swift
+++ AutoLayoutGuide/ParentViewController.swift
@@ -15,11 +15,12 @@ class ParentViewController: UIViewController {
     var blackViewWidth : CGFloat!
     var otherViewsHeight : CGFloat!
     var otherViewsSeparator : CGFloat!
+    var pinkPuPuLabel : UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        blackViewHeight = max(self.view.bounds.height, self.view.bounds.width) * 2/4
-        blackViewWidth =  min(self.view.bounds.height, self.view.bounds.width) * 2/4
+        blackViewHeight = max(self.view.bounds.height, self.view.bounds.width) * 1/2
+        blackViewWidth =  min(self.view.bounds.height, self.view.bounds.width) * 1/2
         
         otherViewsSeparator = 20
         otherViewsHeight = (blackViewHeight/3 - 2*otherViewsSeparator)
@@ -30,12 +31,22 @@ class ParentViewController: UIViewController {
         layoutYellowView()
         layoutBlackView()
         orientationCheck()
+        
+        pinkPuPuLabel = createPuPuLabel()
+        self.view.addSubview(pinkPuPuLabel)
+        pinkPuPuLabel.translatesAutoresizingMaskIntoConstraints = false
+        pinkPuPuLabel.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        pinkPuPuLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        pinkPuPuLabel.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        pinkPuPuLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
     }
     
     override func viewDidLayoutSubviews() {
         orientationCheck()
     }
     
+    
+    //MARK: -Funcs
     func createViews(){
         let viewsArray = [redView, yellowView, greenView]
         let colorsArray : [UIColor] = [.systemRed, .systemYellow, .systemGreen]
@@ -49,7 +60,16 @@ class ParentViewController: UIViewController {
         blackView.backgroundColor = .black
         blackView.translatesAutoresizingMaskIntoConstraints = false
         blackView.layer.cornerRadius = 40
+        //add gesture
+        redView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        redView.addGestureRecognizer(tap)
         self.view.addSubview(blackView)
+        
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+        pinkPuPuLabel.isHidden = !pinkPuPuLabel.isHidden
     }
     
     func layoutBlackView(){
@@ -63,6 +83,48 @@ class ParentViewController: UIViewController {
     
     func layoutGreenView(){
     }
+    
+    func createPuPuLabel() -> UILabel{
+//        let label = UILabel()
+//        label.text = "пупупу блин"
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        label.textColor = .white
+//        label.font = UIFont.systemFont(ofSize: 20)
+//        label.textAlignment = .center
+//        label.numberOfLines = 0
+//        yellowView.addSubview(label)
+//        label.centerXAnchor.constraint(equalTo: yellowView.centerXAnchor).isActive = true
+//        label.centerYAnchor.constraint(equalTo: yellowView.centerYAnchor).isActive = true
+//        label.widthAnchor.constraint(equalToConstant: otherViewsHeight - 10).isActive = true
+//        label.heightAnchor.constraint(equalToConstant: otherViewsHeight - 10).isActive = true
+        
+        let pinkPuPuLabel = UILabel()
+        pinkPuPuLabel.backgroundColor = .systemPink
+        pinkPuPuLabel.textColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        pinkPuPuLabel.text = "пу пу пу \n блин"
+        pinkPuPuLabel.font = UIFont.systemFont(ofSize: 70)
+        pinkPuPuLabel.textAlignment = .center
+        pinkPuPuLabel.numberOfLines = 0
+        pinkPuPuLabel.isHidden = true
+        //autolayout
+        
+        //add gesture
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.pupuTapped(label:)))
+        pinkPuPuLabel.addGestureRecognizer(tap)
+        tap.view?.translatesAutoresizingMaskIntoConstraints = false
+        tap.view?.leftAnchor.constraint(equalTo: pinkPuPuLabel.leftAnchor).isActive = true
+        tap.view?.rightAnchor.constraint(equalTo: pinkPuPuLabel.rightAnchor).isActive = true
+        tap.view?.topAnchor.constraint(equalTo: pinkPuPuLabel.topAnchor).isActive = true
+        tap.view?.bottomAnchor.constraint(equalTo: pinkPuPuLabel.topAnchor).isActive = true
+        
+        
+        return pinkPuPuLabel
+    }
+    
+    @objc func pupuTapped(label: UILabel?){
+        pinkPuPuLabel.isHidden = !pinkPuPuLabel.isHidden
+    }
+
     
     func orientationCheck(){
         if UIDevice.current.orientation.isLandscape{
--- AutoLayoutGuide/VisualFormatLanguageVC.swift
+++ AutoLayoutGuide/VisualFormatLanguageVC.swift
@@ -14,5 +14,85 @@ class VisualFormatLanguageVC : ParentViewController{
         view.backgroundColor = .white
     }
     
+    override func layoutBlackView() {
+        let vflViews : [String : UIView] = ["blackView" : blackView, "superview" : view]
+        let vflMetrics : [String: Any] = ["verticalSpacing"     : view.bounds.height * 1/4,
+                                          "horisontalSpacing"   : view.bounds.width * 1/4,
+                                          "height"              : blackViewHeight!,
+                                          "width"               : blackViewWidth!]
+        
+        let vflConstraintsVertical    = "V:|-(verticalSpacing@999)-[blackView(height)]-(verticalSpacing@999)-|"
+        let vflConstraintsHorisontal  = "H:|-(horisontalSpacing@999)-[blackView(width)]-(horisontalSpacing@999)-|"
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: vflConstraintsVertical,
+                                                           metrics: vflMetrics,
+                                                           views: vflViews))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: vflConstraintsHorisontal,
+                                                           metrics: vflMetrics,
+                                                           views: vflViews))
+        NSLayoutConstraint(item: blackView,
+                           attribute: .centerY,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .centerY,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: blackView,
+                           attribute: .centerX,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .centerX,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+    }
+    
+    override func layoutYellowView() {
+        let vflViews : [String : UIView] = ["redView" : redView, "greenView" : greenView, "yellowView" : yellowView]
+        let vflMetrics = ["spacing" : otherViewsSeparator, "H" : otherViewsHeight]
+        
+        let vflConstraintsVertical = "V:|-(>=0)-[redView(H)]-(spacing)-[yellowView(H)]-(spacing)-[greenView(H)]-(>=0)-|"
+//        strange code with vfl trying to center x
+//        let vflYellowConstraintsHorisontal = "H:|-(>=0)-[yellowView(H)]-(>=0)-|"
+//        let vflGreenConstraintsHorisontal  = "H:|-(>=0)-[greenView(H)]-(>=0)-|"
+//        let vflRedConstraintsHorisontal    = "H:|-(>=0)-[redView(H)]-(>=0)-|"
+        
+        NSLayoutConstraint(item: yellowView,
+                           attribute: .centerY,
+                           relatedBy: .equal,
+                           toItem: blackView,
+                           attribute: .centerY,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: yellowView,
+                           attribute: .centerX,
+                           relatedBy: .equal,
+                           toItem: blackView,
+                           attribute: .centerX,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        //right code with anchors
+        //center x
+        greenView.centerXAnchor.constraint(equalTo: blackView.centerXAnchor).isActive = true
+        redView.centerXAnchor.constraint(equalTo: blackView.centerXAnchor).isActive = true
+        //width
+        redView.widthAnchor.constraint(equalToConstant:otherViewsHeight).isActive = true
+        greenView.widthAnchor.constraint(equalToConstant: otherViewsHeight).isActive = true
+        yellowView.widthAnchor.constraint(equalToConstant: otherViewsHeight).isActive = true
+        
+        
+        blackView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: vflConstraintsVertical,
+                                                                metrics: vflMetrics as [String : Any],
+                                                                views: vflViews))
+//        blackView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: vflRedConstraintsHorisontal,
+//                                                                metrics: vflMetrics as [String : Any],
+//                                                                views: vflViews))
+//        blackView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: vflGreenConstraintsHorisontal,
+//                                                                metrics: vflMetrics as [String : Any],
+//                                                                views: vflViews))
+//        blackView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: vflYellowConstraintsHorisontal,
+//                                                                metrics: vflMetrics as [String : Any],
+//                                                                views: vflViews))
+    }
     
 }
